468b08da96f627bd7a317996ff1561390e9cfc14
seungmin <746aa1357@gmail.com>
2024-08-26 09:05:37 +0900

    information write v4

--- static/js/information.js
+++ static/js/information.js
@@ -2,29 +2,35 @@ const textareas = document.querySelectorAll(".moa-textarea");
 const attachButton = document.querySelector(".attach-button");
 const thumbnail = document.querySelector(".thumbnail");
 const cancelButton = document.querySelector(".cancel-button");
+const postContent = document.querySelector("#content-textarea");
+const wordLength = document.querySelector(".word-length");
+const helpText = document.querySelector(".help-text");
+const maxWordLength = 5000;
 
 textareas.forEach((textarea) => {
     let value = textarea.value;
     let focusCheck = false;
     textarea.addEventListener("mouseenter", (e) => {
-        if (!e.target.parentElement.className.includes("changed")) {
-            e.target.parentElement.className = "textarea-border-changed";
-        }
+        // if (!e.target.parentElement.className.includes("changed")) {
+        e.target.parentElement.className = "textarea-border-changed";
+        // }
     });
     textarea.addEventListener("mouseleave", (e) => {
-        if (e.target.parentElement.className.includes("changed")) {
-            if (focusCheck) {
-                e.target.parentElement.className = "textarea-border-changed";
-            } else {
-                e.target.parentElement.className = "textarea-border";
-            }
+        // if (e.target.parentElement.className.includes("changed")) {
+        if (focusCheck) {
+            e.target.parentElement.className = "textarea-border-changed";
+        } else {
+            e.target.parentElement.className = "textarea-border";
         }
+        // }
     });
     textarea.addEventListener("focus", (e) => {
         focusCheck = true;
         if (e.target.parentElement.className.includes("changed")) {
             e.target.parentElement.className = "textarea-border-changed";
-            e.target.value = "";
+            if (e.target.value == value) {
+                e.target.value = "";
+            }
         } else {
             e.target.parentElement.className = "textarea-border";
             e.target.value = value;
@@ -37,10 +43,24 @@ textareas.forEach((textarea) => {
             e.target.value = "";
         } else {
             e.target.parentElement.className = "textarea-border";
-            e.target.value = value;
+            if (!e.target.value) {
+                e.target.value = value;
+            }
         }
     });
 });
+
+wordLength.innerText = `${postContent.value.length}/${maxWordLength}`;
+postContent.addEventListener("click", (e) => {
+    wordLength.innerText = `${postContent.value.length}/${maxWordLength}`;
+});
+postContent.addEventListener("keyup", (e) => {
+    wordLength.innerText = `${postContent.value.length}/${maxWordLength}`;
+});
+postContent.addEventListener("blur", (e) => {
+    wordLength.innerText = `${postContent.value.length}/${maxWordLength}`;
+});
+helpText.innerText = `${maxWordLength}자 이내로 작성해주세요.`;
 
 // attachButton.addEventListener("click", (e) => {
 //     const [file] = e.target.files;
