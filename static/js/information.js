f48de07f413ae9c67f70f1afdeebec3ab8ae002e
seungmin <746aa1357@gmail.com>
2024-08-26 18:32:53 +0900

    information write attach almost completed

--- static/js/information.js
+++ static/js/information.js
@@ -6,10 +6,10 @@ const maxWordLength = 5000;
 const attachWrap = document.querySelector(".attach-wrap");
 const attachButton = document.querySelector(".attach-button");
 const attachInput = document.querySelector("#attach-input");
-const thumbnail = document.querySelector(".thumbnail");
-const attachCancelButton = document.querySelector(".attach-cancel-button");
 const attachFilesWrap = document.querySelector(".attach-files-wrap");
+let i = 0;
 
+// mouseenter 및 focus가 되었을 때 스타일 적용
 textareas.forEach((textarea) => {
     let value = textarea.value;
     let focusCheck = false;
@@ -53,6 +53,7 @@ textareas.forEach((textarea) => {
     });
 });
 
+// 현재 게시글 글자수와 최대 글자수 계산
 wordLength.innerText = `${postContent.value.length}/${maxWordLength}`;
 helpText.innerText = `${maxWordLength}자 이내로 작성해주세요.`;
 postContent.addEventListener("click", (e) => {
@@ -69,32 +70,58 @@ attachInput.addEventListener("change", (e) => {
     const [file] = e.target.files;
     const reader = new FileReader();
     reader.readAsDataURL(file);
+    const attachFile = `<div class="attach-file">
+                            <label>
+                                    <div class="attach-cancel-button" id=attach-cancel-button-${++i}></div>
+                                    <input type="file" id=attach-check-${i} disabled/>
+                            </label>
+                            <div class="thumbnail-wrap white-box">
+                                <div class="thumbnail" id=thumbnail-${i}></div>
+                            </div>
+                            <div>${file.name}</div>
+                        </div>`;
+    // const attachCheckUnique = document.querySelector(`#attach-check-${i}`);
+    // console.log(attachCheckUnique);
+    // attachCheckUnique.files = file;
     reader.addEventListener("load", (e) => {
         const path = e.target.result;
+        const thumbnailUnique = document.querySelector(`#thumbnail-${i}`);
+        const attachCancelButtonUnique = document.querySelector(
+            `#attach-cancel-button-${i}`
+        );
         if (path.includes("image")) {
-            thumbnail.style.backgroundImage = `url(${path})`;
-            attachCancelButton.style.display = "block";
+            console.log(i);
+            thumbnailUnique.style.backgroundImage = `url(${path})`;
+            attachCancelButtonUnique.style.display = "block";
         } else {
-            thumbnail.style.backgroundImage = "";
+            thumbnailUnique.style.backgroundImage = "";
         }
     });
-    const attachFile = `<div class="attach-file">
-                            ${file.name}
-                            <label>
-                                    <div>
-                                        <img
-                                        src="../../../static/images/cancel3.png"
-                                        alt="attach-file-image"
-                                        />
-                                    </div>
-                                    <input type="checkbox" class="attach-check" />
-                            </label>
-                        </div>`;
+    const attachCancelButtons = document.querySelectorAll(
+        ".attach-cancel-button"
+    );
+    const thumbnails = document.querySelectorAll(".thumbnail");
+    attachCancelButtons.forEach((attachCancelButton) => {
+        attachCancelButton.addEventListener("click", (e) => {
+            thumbnail.style.backgroundImage = "";
+            attachInput.value = "";
+            attachCancelButton.style.display = "none";
+        });
+    });
     attachFilesWrap.innerHTML += attachFile;
 });
-
-attachCancelButton.addEventListener("click", (e) => {
-    thumbnail.style.backgroundImage = "";
-    attachInput.value = "";
-    attachCancelButton.style.display = "none";
-});
+// else {
+//     // 체크가 해제되었다면,
+//     // 기존에 p태그에 있었던 모든 span태그를 가져온다.
+//     const spans = document.querySelectorAll("span.text");
+//     // span태그 중,
+//     // 내용이 현재 해제한 체크박스의 value와 다른 것만 추출한다.
+//     spans
+//         .filter((span) => span.innerText !== e.target.value)
+//         .forEach((span) => {
+//             // 추출된 span태그를 다시 제작한다.
+//             text += `<span class="text">${span.innerText}</span>`;
+//         });
+//     // 완성된 span태그들을 result에 덮어쓴다.
+//     result.innerHTML = text;
+// }
