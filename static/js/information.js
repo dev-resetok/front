17384842567f881198edf704f9e3578201f40225
seungmin <746aa1357@gmail.com>
2024-08-27 14:20:57 +0900

    information write 95%

--- static/js/information.js
+++ static/js/information.js
@@ -9,6 +9,7 @@ const attachInput = document.querySelector("#attach-input");
 const attachFilesWrap = document.querySelector(".attach-files-wrap");
 let i = 0;
 
+FileReader.prototype.append = Array.prototype.append;
 NodeList.prototype.filter = Array.prototype.filter;
 
 // mouseenter 및 focus가 되었을 때 스타일 적용
@@ -70,10 +71,11 @@ postContent.addEventListener("blur", (e) => {
 
 attachInput.addEventListener("change", (e) => {
     const [file] = e.target.files;
-    console.log(file);
-    console.log(e.target);
+    // console.log(file);
+    // console.log(e.target);
     const reader = new FileReader();
     reader.readAsDataURL(file);
+    // console.log(file);
     const attachFile = `<div class="attach-file">
                             <label>
                                     <div class="attach-cancel-button" id=attach-cancel-button-${++i}></div>
@@ -87,54 +89,29 @@ attachInput.addEventListener("change", (e) => {
     attachFilesWrap.innerHTML += attachFile;
 
     const attachInfo = document.querySelector(`#attach-info-${i}`);
-    console.log(attachInfo);
-    attachInfo.files = e.target.files;
-    console.log(attachInfo.files);
-    console.log(e.target.value);
-    console.log(attachInput.value);
+    const dataTransfer = new DataTransfer();
+
+    dataTransfer.items.add(file);
+    attachInfo.files = dataTransfer.files;
+    // console.log(attachInfo.files);
+    // console.log(i);
     attachInput.value = "";
-    console.log(attachInput.value);
-    console.log(e.target.value);
+
+    const thumbnailUnique = document.querySelector(`#thumbnail-${i}`);
+    const attachCancelButtonUnique = document.querySelector(
+        `#attach-cancel-button-${i}`
+    );
+    attachCancelButtonUnique.addEventListener("click", (e) => {
+        attachFilesWrap.removeChild(e.target.parentElement.parentElement);
+    });
     reader.addEventListener("load", (e) => {
         const path = e.target.result;
-        const thumbnailUnique = document.querySelector(`#thumbnail-${i}`);
-        const attachCancelButtonUnique = document.querySelector(
-            `#attach-cancel-button-${i}`
-        );
         if (path.includes("image")) {
-            // console.log(i);
             thumbnailUnique.style.backgroundImage = `url(${path})`;
             attachCancelButtonUnique.style.display = "block";
         } else {
             thumbnailUnique.style.backgroundImage = "";
             attachCancelButtonUnique.style.display = "block";
         }
-    });
-    const attachCancelButtons = document.querySelectorAll(
-        ".attach-cancel-button"
-    );
-    // const thumbnails = document.querySelectorAll(".thumbnail");
-    attachCancelButtons.forEach((attachCancelButton) => {
-        attachCancelButton.addEventListener("click", (e) => {
-            let temp = 0;
-            // thumbnail.style.backgroundImage = "";
-            attachInput.value = "";
-            // attachCancelButton.style.display = "none";
-            const attachFiles = document.querySelectorAll(".attach-file");
-            // span태그 중,
-            // 내용이 현재 해제한 체크박스의 value와 다른 것만 추출한다.
-            attachFiles
-                .filter(
-                    (attachFile) =>
-                        attachFile.firstElementChild.firstElementChild.files !==
-                        e.target.files
-                )
-                .forEach((attachFile) => {
-                    // 추출된 span태그를 다시 제작한다.
-                    temp += attachFile;
-                });
-            // 완성된 span태그들을 result에 덮어쓴다.
-            attachFilesWrap.innerHTML = temp;
-        });
     });
 });
