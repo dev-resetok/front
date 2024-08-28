887696a7b63b1bde017683cc1014797d3222d183
seungmin <746aa1357@gmail.com>
2024-08-28 16:36:14 +0900

    write pages all completed maybe

--- static/js/information/information_write.js
+++ static/js/information/information_write.js
@@ -3,10 +3,7 @@ const postContent = document.querySelector("#content-textarea");
 const wordLength = document.querySelector(".word-length");
 const helpText = document.querySelector(".help-text");
 const maxWordLength = 5000;
-const attachWrap = document.querySelector(".attach-wrap");
-const attachButton = document.querySelector(".attach-button");
-const attachInput = document.querySelector("#attach-input");
-const attachFilesWrap = document.querySelector(".attach-files-wrap");
+const attachInputs = document.querySelectorAll(".attach-input");
 const saveButton = document.querySelector(".save-button");
 let i = 0;
 
@@ -73,57 +70,66 @@ postContent.addEventListener("blur", (e) => {
 });
 
 // 첨부파일 목록 관리
-// 첨부파일 버튼이 클릭되어 attachInput에 file이 입력되었을 때
-attachInput.addEventListener("change", (e) => {
-    // 파일 정보 불러오기
-    const [file] = e.target.files;
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-    // 첨부파일 객체 생성
-    const attachFile = `<div class="attach-file">
-                            <label>
-                                    <div class="attach-cancel-button" id=attach-cancel-button-${++i}></div>
-                                    <input type="file" id=attach-info-${i} disabled/>
-                            </label>
-                            <div class="thumbnail-wrap white-box">
-                                <div class="thumbnail" id=thumbnail-${i}></div>
-                            </div>
-                            <div class="attach-text-wrap">${file.name}</div>
-                        </div>`;
-    // 생성된 첨부파일 객체를 첨부파일 목록에 추가
-    attachFilesWrap.innerHTML += attachFile;
-    // attachInput에 담긴 파일 정보를 첨부파일 객체의 files에 추가
-    const attachInfo = document.querySelector(`#attach-info-${i}`);
-    const dataTransfer = new DataTransfer();
-    dataTransfer.items.add(file);
-    attachInfo.files = dataTransfer.files;
-    // attachInput에 담긴 파일 정보 삭제
-    attachInput.value = "";
-    // 첨부파일 삭제버튼 및 입력된 첨부파일의 종류에 따른 썸네일 생성
-    reader.addEventListener("load", (e) => {
-        const thumbnailUnique = document.querySelector(`#thumbnail-${i}`);
-        const attachCancelButtonUnique = document.querySelector(
+attachInputs.forEach((attachInput) => {
+    // 첨부파일 버튼이 클릭되어 attachInput에 file이 입력되었을 때
+    attachInput.addEventListener("change", (e) => {
+        // 파일 정보 불러오기
+        const [file] = e.target.files;
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        // 첨부파일 객체 생성
+        const attachFile = `<div class="attach-file">
+                                <label>
+                                        <div class="attach-cancel-button" id=attach-cancel-button-${++i}></div>
+                                        <input type="file" id=attach-info-${i} disabled/>
+                                </label>
+                                <div class="thumbnail-wrap white-box">
+                                    <div class="thumbnail" id=thumbnail-${i}></div>
+                                </div>
+                                <div class="attach-text-wrap">${file.name}</div>
+                            </div>`;
+        const attachButtonFileWrap =
+            attachInput.parentElement.parentElement.parentElement;
+        const attachButton = attachButtonFileWrap.firstElementChild;
+        attachButtonFileWrap.innerHTML += attachFile;
+        // attachInput에 담긴 파일 정보를 첨부파일 객체의 files에 추가
+        const attachInfo = document.querySelector(`#attach-info-${i}`);
+        const dataTransfer = new DataTransfer();
+        dataTransfer.items.add(file);
+        attachInfo.files = dataTransfer.files;
+        // attachInput에 담긴 파일 정보 삭제 및 attachButton 삭제
+        attachInput.value = "";
+        attachButtonFileWrap.removeChild(
+            attachButtonFileWrap.firstElementChild
+        );
+        // 첨부파일 삭제버튼 및 입력된 첨부파일의 종류에 따른 썸네일 생성
+        reader.addEventListener("load", (e) => {
+            const thumbnailUnique = document.querySelector(`#thumbnail-${i}`);
+            const attachCancelButtonUnique = document.querySelector(
+                `#attach-cancel-button-${i}`
+            );
+            const path = e.target.result;
+            // 이미지 파일 썸네일 생성
+            if (path.includes("image")) {
+                thumbnailUnique.style.backgroundImage = `url(${path})`;
+                attachCancelButtonUnique.style.display = "block";
+                //  기타 파일은 빈 썸네일
+            } else {
+                thumbnailUnique.style.backgroundImage = "";
+                attachCancelButtonUnique.style.display = "block";
+            }
+        });
+        // 모든 첨부파일의 삭제버튼에 해당 파일 삭제기능 추가
+        const attachCancelButton = document.querySelector(
             `#attach-cancel-button-${i}`
         );
-        const path = e.target.result;
-        // 이미지 파일 썸네일 생성
-        if (path.includes("image")) {
-            thumbnailUnique.style.backgroundImage = `url(${path})`;
-            attachCancelButtonUnique.style.display = "block";
-            //  기타 파일은 빈 썸네일
-        } else {
-            thumbnailUnique.style.backgroundImage = "";
-            attachCancelButtonUnique.style.display = "block";
-        }
-    });
-    // 모든 첨부파일의 삭제버튼에 해당 파일 삭제기능 추가
-    const attachCancelButtons = document.querySelectorAll(
-        ".attach-cancel-button"
-    );
-    attachCancelButtons.forEach((attachCancelButton) => {
         attachCancelButton.addEventListener("click", (e) => {
             // 첨부파일 목록에서 해당 파일 삭제
-            attachFilesWrap.removeChild(e.target.parentElement.parentElement);
+            attachInfo.value = "";
+            attachButtonFileWrap.appendChild(attachButton);
+            attachButtonFileWrap.removeChild(
+                attachButtonFileWrap.firstElementChild
+            );
         });
     });
 });
