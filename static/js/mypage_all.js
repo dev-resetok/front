96d82370285775cc37c00b0a1e626ff55f93745e
jimin9712 <ljm21000@gmail.com>
2024-08-28 10:24:26 +0900

    mypage

--- static/js/mypage_all.js
+++ static/js/mypage_all.js
@@ -23,23 +23,29 @@ document.addEventListener("click", () => {
     const editableElements = document.querySelectorAll(".data-editable");
 
     editableElements.forEach((element) => {
+        // 각 요소에 대해 반복적으로 처리하는 코드 블록을 정의
         element.addEventListener("click", () => {
             const currentText = element.innerText;
             const input = document.createElement("input");
             input.type = "text";
+            // 생성된 입력 필드의 유형을 text로 설정
+
             input.value =
                 currentText.trim() === "정보를 입력해주세요."
                     ? ""
                     : currentText.trim();
+            // 만약 현재 텍스트가 "정보를 입력해주세요."인 경우, 입력 필드를 비워두고
+            // 그렇지 않으면, 기존 텍스트를 입력 필드에 표시
+            // `trim()` 메서드는 텍스트의 앞뒤 공백 제거
             element.innerHTML = "";
             element.appendChild(input);
             input.focus();
-
             input.addEventListener("blur", () => {
                 const newText =
                     input.value.trim() === ""
                         ? "정보를 입력해주세요."
                         : input.value.trim();
+                // 만약 입력 필드가 비어 있다면, 기본 메시지("정보를 입력해주세요.")를 사용
                 element.innerText = newText;
             });
 
