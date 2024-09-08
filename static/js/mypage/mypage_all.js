a142c84d6fc2455336d6e973c1712d54aacf6960
jimin9712 <ljm21000@gmail.com>
2024-09-08 23:39:51 +0900

    point

--- static/js/mypage/mypage_all.js
+++ static/js/mypage/mypage_all.js
@@ -1253,6 +1253,13 @@ const monthButtons = document.querySelectorAll("#monthListArea2 li a");
 
 monthButtons.forEach((button) => {
     button.addEventListener("click", function () {
+        // 모든 버튼에서 'selected' 클래스를 제거
+        monthButtons.forEach((btn) => btn.classList.remove("selected"));
+
+        // 클릭된 버튼에 'selected' 클래스 추가
+        this.classList.add("selected");
+
+        // 날짜 범위 설정
         const month = this.parentElement.getAttribute("value");
         setDefaultDateRange(month);
     });
